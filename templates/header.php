20f9aa7f989e46b63421aba448849efed1d42c24
paulorobertom <paulo1rm23@gmail.com>
2015-05-06 10:02:25 -0300

    Polpa Toda Boa
    Primeiro Commit do Site da Polpa Toda Boa. Site iniciado.

---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="description" content="Agência dos Mares - Comunicação Digital." />
+		<meta name="keywords" content="Código Limpo, Desenvolvimento Web, HTML5, Desenvolvimento Ágil" />
+		<meta name="author" content="Agência dos Mares" />
+		<meta name="reply-to" content="contato@agenciadosmares.com.br" />
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/my-style.css" />	    
+	</head>
+	<body>
+		<div class="row-fluid">
+			<header>
+
+			</header> <!-- Close header -->
+		</div> <!-- Close row-fluid -->
+
+		<!-- ************ Scripts ********** -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+  		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>  		
+  		<script type="text/javascript" src="js/gmaps.js"></script>
+  		<script type="text/javascript" src="js/my-functios.js"></script>
+  		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script>
+	        $('#my-carousel').carousel({interval: 3000})
+	    </script>
+	</body>
+</html>
