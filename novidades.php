449d19cb3b2b9795a460dd67c4c1abd49a4be306
paulorobertom <paulo1rm23@gmail.com>
2015-05-06 23:20:00 -0300

    Finalização Parcial
    Inclusão de todas as sub-páginas do site. Site finalizado, aguardando as
    imagens.

---
+++ novidades.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Novidades</title>
+	</head>
+	<body>
+		
+		<!-- ********* Chamada do header.PHP ********* -->
+		<?php include("templates/header.php"); ?>
+
+		<section>				
+			<div class="row">
+				<div class="container">
+					<h3 class="my-produto-titulo"> > Confira Nossas Novidades</h3>
+					<div class="row">
+						<div class="thumbnail my-produto">
+							<h2>Cajá</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+						<div class="thumbnail my-produto">
+							<h2>Maracujá</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+						<div class="thumbnail my-produto">
+							<h2>Uva</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+						<div class="thumbnail my-produto">
+							<h2>Morango</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="thumbnail my-produto">
+							<h2>Cajá</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+						<div class="thumbnail my-produto">
+							<h2>Maracujá</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+						<div class="thumbnail my-produto">
+							<h2>Uva</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+						<div class="thumbnail my-produto">
+							<h2>Morango</h2>
+							<img src="img/Pizza.png" alt="Produtos - Polpas">
+							<p>Lorem ipsum dolor sit amet</p>
+							<p>Preço: R$ 00,00</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<!-- ******** Chamada do footer.PHP ******** -->
+		<?php include("templates/footer.php"); ?>
+		
+	</body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Sobre</title>
+	</head>
+	<body>
+		
+		<!-- ********* Chamada do header.PHP ********* -->
+		<?php include("templates/header.php"); ?>
+
+		<section>				
+			<div class="row">
+				<div class="container">
+					<div class="span6 my-polpa">
+						<h2>Polpa Toda Boa</h2>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa.</p>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa.</p>
+					</div>
+					<div class="span4 my-polpa-img">
+						<img src="img/Logo.png" alt="Polpa Toda Boa" />
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<!-- ******** Chamada do footer.PHP ******** -->
+		<?php include("templates/footer.php"); ?>
+		
+	</body>
+</html>
